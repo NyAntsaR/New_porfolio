1016f1eeae01044624345889e91f21b3414e6980
andy ratsirarson <andy.ratsirarson@andys-MacBook-Air.local>
2020-02-17 07:41:54 -0800

    Add particles

--- js/myparticles.js
+++ js/myparticles.js
@@ -1,21 +1,21 @@
 particlesJS("myparticles", {
   particles: {
-    number: { value: 30, density: { enable: true, value_area: 800 } },
+    number: { value: 50, density: { enable: true, value_area: 200 } },
     color: { value: "#ffffff" },
     shape: {
-      type: "circle",
+      type: "polygon",
       stroke: { width: 0, color: "#000000" },
       polygon: { nb_sides: 5 },
       image: { src: "img/github.svg", width: 100, height: 100 }
     },
     opacity: {
-      value: 0.3,
+      value: 0.5,
       random: false,
       anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
     },
     size: {
       value: 5,
-      random: false,
+      random: true,
       anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
     },
     line_linked: {
