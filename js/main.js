f83523017a3f7a7459633bc4df02654317969fa9
andy ratsirarson <andy.ratsirarson@andys-air.lan>
2020-02-18 08:10:26 -0800

    About section

--- js/main.js
+++ js/main.js
@@ -9,5 +9,20 @@ doc.ready(function() {
         loop: true,
         typeSpeed: 70,
         backDelay: 1000
-    })
+    });
+
+    
+
+    $(window).scroll(function() {
+        var top = $(window).scrollTop();
+
+        if (top>=200){
+            $('header').addClass('overlay');
+            $('.navbar-nav').removeClass('navbar-nav-bg');
+        } else if ($('header').hasClass('overlay')) {
+            $('header').removeClass('overlay');
+        } else {
+            $('.navbar-nav').addClass('navbar-nav-bg');
+        }
+    });
 })
